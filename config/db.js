0e4a8a7b0ff11cc9ca25d1dedb014c6ee418c3d5
Ahssan23 <ahssan180sx@gmail.com>
2026-08-27 18:25:58 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.DB_HOST) {
   console.error("FATAL: DATABASE_URL environment variable is missing.");
 }
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_HOST,
   ssl: process.env.NODE_ENV === 'production' 
     ? { rejectUnauthorized: false } 
     : false,
